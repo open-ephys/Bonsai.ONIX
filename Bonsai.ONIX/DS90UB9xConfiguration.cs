6623e25ba0e0da90f17a97aa44a2c4f2821e384f
Aaron Cuevas Lopez <aacuelo@teleco.upv.es>
2021-07-07 19:20:16 +0200

    Add class for raw DS90UBx device

---
+++ Bonsai.ONIX/DS90UB9xConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonsai.ONIX
+{
+    public static class DS90UB9xConfiguration
+    {
+        public enum Register
+        {
+            //Managed register map
+            ENABLE = 0x00008000,
+            READSZ = 0x00008001,
+            TRIGGER = 0x00008002,
+            TRIGGER_OFFSET = 0x00008003,
+            PIXEL_GATE = 0x00008004,
+            SYNC_BITS = 0x00008005,
+            GPIO_DIR = 0x00008006,
+            GPIO_VALUE = 0x00008007
+        }
+
+        public enum TriggerMode
+        {
+            CONTINUOUS = 0,
+            HSYNC_EDGE_POSITIVE = 0b0001,
+            HSYNC_EDGE_NEGATIVE = 0b1001,
+            HSYNC_LEVEL_POSITIVE = 0b0101,
+            HSYNC_LEVEL_NEGATIVE = 0b1101,
+            VSYNC_EDGE_POSITIVE = 0b0011,
+            VSYNC_EDGE_NEGATIVE = 0b1011,
+            VSYNC_LEVEL_POSITIVE = 0b0111,
+            VSYNC_LEVEL_NEGATIVE = 0b1111,
+        }
+
+        public enum PixelGate
+        {
+            DISABLED = 0,
+            HSYNC_POSITIVE = 0b001,
+            HSYNC_NEGATIVE = 0b101,
+            VSYNC_POSITIVE = 0b011,
+            VSYNC_NEGATIVE = 0b111
+        }
+
+    }       
+}
---
+++ Bonsai.ONIX/DS90UB9xDevice.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace Bonsai.ONIX
+{
+    public class DS90UB9xDevice : ONIFrameReader<RawDataFrame, ushort>
+    {
+        public DS90UB9xDevice() : base(ONIXDevices.ID.DS90UB9X) { }
+
+        protected override IObservable<RawDataFrame> Process(IObservable<ONIManagedFrame<ushort>> source)
+        {
+            return source.Select(f => { return new RawDataFrame(f); });
+        }
+
+
+        [Category("Configuration")]
+        [Description("Enable the device data stream.")]
+        public bool EnableStream
+        {
+            get
+            {
+                return ReadRegister((uint)DS90UB9xConfiguration.Register.ENABLE) > 0;
+            }
+            set
+            {
+                WriteRegister((uint)DS90UB9xConfiguration.Register.ENABLE, value ? (uint)1 : 0);
+            }
+        }
+
+        [Category("Configuration")]
+        [Description("Frame width in samples (pixels)")]
+        public uint DataSize
+        {
+            get
+            {
+                return ReadRegister((uint)DS90UB9xConfiguration.Register.READSZ);
+            }
+            set
+            {
+                WriteRegister((uint)DS90UB9xConfiguration.Register.READSZ, value);
+            }
+        }
+
+        [Category("Configuration")]
+        [Description("Frame start trigger mode")]
+        public DS90UB9xConfiguration.TriggerMode TriggerMode
+        {
+            get
+            {
+                return (DS90UB9xConfiguration.TriggerMode)ReadRegister((uint)DS90UB9xConfiguration.Register.TRIGGER);
+            }
+            set
+            {
+                WriteRegister((uint)DS90UB9xConfiguration.Register.TRIGGER, (uint)value);
+            }
+        }
+
+        [Category("Configuration")]
+        [Description("Trigger Offset. Start frame N samples after trigger.")]
+        public uint TriggerOffset
+        {
+            get
+            {
+                return ReadRegister((uint)DS90UB9xConfiguration.Register.TRIGGER_OFFSET);
+            }
+            set
+            {
+                WriteRegister((uint)DS90UB9xConfiguration.Register.TRIGGER_OFFSET, value);
+            }
+        }
+
+        [Category("Configuration")]
+        [Description("Pixel gate. Ignore pixels when gate not active.")]
+        public DS90UB9xConfiguration.PixelGate PixelGate
+        {
+            get
+            {
+                return (DS90UB9xConfiguration.PixelGate)ReadRegister((uint)DS90UB9xConfiguration.Register.PIXEL_GATE);
+            }
+            set
+            {
+                WriteRegister((uint)DS90UB9xConfiguration.Register.PIXEL_GATE, (uint)value);
+            }
+        }
+
+        [Category("Configuration")]
+        [Description("Invlude synchronozation bits in samples.")]
+        public bool SyncBits
+        {
+            get
+            {
+                return ReadRegister((uint)DS90UB9xConfiguration.Register.SYNC_BITS) > 0;
+            }
+            set
+            {
+                WriteRegister((uint)DS90UB9xConfiguration.Register.SYNC_BITS, value ? (uint)1 : 0);
+            }
+        }
+    }
+}
