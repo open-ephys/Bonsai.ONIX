2dfa61b05524008560907fe58cc2763ab8c6485e
jonnew <jpnewman@mit.edu>
2021-10-28 07:15:43 -0700

    Update Example Workflows
    - The FrameClock and DataClock members of the output data types of all
    FrameReader nodes was changed to Clock and HubSyncCounter, respectively
    in 594d2d9d5388289e63d629120d8d79d9970f2a37. The example workflows were
    updated to reflect this change.
    - Changed the FrameClock and DataClock members of Position3D to Clock
    and HubSyncCounter, respectively.
    - Added complete Headstage64 example workflow along with matlab script
    for loading data
    - Added SVGs of each example workflow so that documentation can use a,
    hopefully, up to date image of each workflow

--- Bonsai.ONIX/Position3D.cs
+++ Bonsai.ONIX/Position3D.cs
@@ -4,17 +4,17 @@ namespace Bonsai.ONIX
 {
     public class Position3D
     {
-        public Position3D(ulong frameClock, ulong dataClock, Mat position)
+        public Position3D(ulong clock, ulong hubSyncCounter, Mat position)
         {
-            DataClock = dataClock;
-            FrameClock = frameClock;
+            Clock = clock;
+            HubSyncCounter = hubSyncCounter;
             Matrix = position;
             Point = new Point3d(position[0].Val0, position[1].Val0, position[2].Val0);
         }
 
-        public ulong FrameClock { get; private set; }
+        public ulong Clock { get; private set; }
 
-        public ulong DataClock { get; private set; }
+        public ulong HubSyncCounter { get; private set; }
 
         public Point3d Point { get; private set; }
 
